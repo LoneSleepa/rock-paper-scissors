a5dc03f9a4b22e9b93290a8ad282e9a84d8fc5ad
Gill <moroumenos@gmail.com>
2022-08-08 19:33:20 +0900

    modify getComputer function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const rock = 'Rock';
-const paper = 'Paper';
-const scissors = 'Scissors';
+const rock = 'rock'
+const paper = 'paper';
+const scissors = 'scissors';
 const choice = [rock, paper, scissors];
 
-let getComputerChoice = choice[Math.floor(Math.random()*choice.length)];
+function getComputerChoice() {
+const getComputerChoice = choice[Math.floor(Math.random()*choice.length)];
 console.log(getComputerChoice);
-
+}
+getComputerChoice();
