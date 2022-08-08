a596852bfccd3066d8b59832d4cf6f7c6c7d32b7
Gill <moroumenos@gmail.com>
2022-08-08 20:34:33 +0900

    create first draft of playerRound function

--- script.js
+++ script.js
@@ -7,4 +7,33 @@ function getComputerChoice() {
 const getComputerChoice = choice[Math.floor(Math.random()*choice.length)];
 console.log(getComputerChoice);
 }
-getComputerChoice();
+
+
+
+function playerRound (playerSelection, computerSelection) {
+    if (playerSelection === rock &&  computerSelection === scissors) {
+         return win;
+        } else if (playerSelection === scissors && computerSelection === paper) {
+            return win;
+        } else if (playerSelection === paper && computerSelection === rock) {
+           return win;
+        } else if (playerSelection === rock && computerSelection === rock) {
+           return tie;
+        } else if (playerSelection === paper && computerSelection === paper) {
+           return tie;
+        } else if (playerSelection === scissors && computerSelection === scissors) {
+           return tie;
+        } else {
+            return lose;            
+        }    
+}
+
+const playerSelection = rock;
+console.log(playerSelection);
+const computerSelection = getComputerChoice();
+const win = "Congrats! One point for you!";
+const lose = "Sorry, no point for you...";
+const tie = "Tie, reroll";
+
+playerRound(playerSelection, computerSelection);
+console.log(playerRound(playerSelection, computerSelection));
