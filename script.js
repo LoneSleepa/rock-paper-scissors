d5f3d3c42b361fbddde09b4005adf1915f31ebfb
Gill <moroumenos@gmail.com>
2022-08-09 22:35:33 +0900

    added game() conditionals

--- script.js
+++ script.js
@@ -4,18 +4,15 @@ const scissors = 'scissors';
 const choice = [rock, paper, scissors];
 
 const playerSelection = rock;
-console.log(playerSelection);
 const computerSelection = getComputerChoice();
 const win = "Congrats! One point for you!";
 const lose = "Sorry, no point for you...";
 const tie = "Tie, reroll";
-
-
+const endgame = 5;
 
 function getComputerChoice() {
 const getComputerChoice = choice[Math.floor(Math.random()*choice.length)];
 return getComputerChoice;
-console.log(getComputerChoice);
 }
 
 
@@ -38,27 +35,46 @@ function playerRound (playerSelection, computerSelection) {
         }    
 }
 
-
-
-playerRound(playerSelection, computerSelection);
-console.log(playerRound(playerSelection, computerSelection));
-
 let playerScore = 0;
 let computerScore = 0;
-console.log(playerScore);
-console.log(computerScore);
 
 function game() {
-    if (win) {
-        playerScore = playerScore++;
-        return playerScore++;
-    } else {
-        computerScore = computerScore++;
-        return computerScore++;
-    }
     playerRound(playerSelection, computerSelection);
+
+    for (let i= 0; i < (playerScore === 5 || computerScore === 5); i++) {
+        console.log(i);
+    }
+
+
+    if (playerSelection === rock &&  computerSelection === scissors) {
+        playerScore += 1;
+        console.log(playerScore);
+        return playerScore;
+    } else if (playerSelection === scissors && computerSelection === paper) {
+        playerScore += 1;
+        console.log(playerScore);
+        return playerScore;
+    } else if (playerSelection === paper && computerSelection === rock) {
+        playerScore += 1;
+        console.log(playerScore);
+        return playerScore;
+    } else if (playerSelection === paper && computerSelection === scissors) {
+        computerScore += 1;
+        console.log(computerScore);
+        return computerScore;
+    } else if (playerSelection === rock && computerSelection === paper) {
+        computerScore += 1;
+        console.log(computerScore);
+        return computerScore;
+    } else if (playerSelection === scissors && computerSelection === rock) {
+        computerScore += 1;
+        console.log(computerScore);
+        return computerScore;
+    }
 }
 
 game();
 console.log(playerScore);
 console.log(computerScore);
+console.log(playerSelection, computerSelection);
+console.log
